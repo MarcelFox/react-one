0e60dd52144887b5c4259dbc35a55c5135b39da8
Marcel Esteves <marcel.esteves@zpesystems.com>
2022-01-25 20:23:29 -0300

    upgrading react route & adjusting structure

--- src/domains/example/home/components/PageNotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class PageNotFound extends React.PureComponent {
-  render() {
-    return <h1>404 Page Not Found</h1>;
-  }
-}
-
-export default PageNotFound;
---
+++ src/domains/landingPage/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+export default () => (
+  <frameElement>
+    <nav
+      style={{
+        borderBottom: 'solid 1px',
+      }}
+    >
+      <ul>
+        <li style={{ display: 'inline', padding: 5 }}>
+          <Link to="/">Home</Link>
+        </li>
+        <li
+          style={{
+            display: 'inline',
+            padding: 5,
+            borderLeft: '1px solid black',
+          }}
+        >
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+    <Outlet />
+  </frameElement>
+);
