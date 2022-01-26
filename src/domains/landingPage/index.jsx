fb53a7bf6f60042d7d32b671bcc28bcac6371b24
Marcel Esteves <marcel.esteves@zpesystems.com>
2022-01-26 10:13:55 -0300

    Adjusting domains structure

--- src/domains/landingPage/index.jsx
+++ src/domains/landingPage/index.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import NavBar from '@Domain/landingPage/components/navBar';
 
 export default () => (
   <frameElement>
-    <nav
-      style={{
-        borderBottom: 'solid 1px',
-      }}
-    >
-      <ul>
-        <li style={{ display: 'inline', padding: 5 }}>
-          <Link to="/">Home</Link>
-        </li>
-        <li
-          style={{
-            display: 'inline',
-            padding: 5,
-            borderLeft: '1px solid black',
-          }}
-        >
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
+    <NavBar />
     <Outlet />
   </frameElement>
 );
