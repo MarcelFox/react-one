d2bca2522b1ecda00c59fffd6e61d9da665b2281
Marcel Cattaneo <marcel.cattaneo@zpesystems.com>
2022-02-21 07:08:52 -0300

    adjusting routes

--- webpack.client.js
+++ webpack.client.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   output: {
     filename: '[name]/bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     extensions: ['.js', '.jsx'],
